904568828dc49be45e1a062c393351740aaa902e
Denis <denis_kondratev_2011@mail.ru>
2026-04-27 21:21:26 +0400

    3. скорректирован SECRET_KEY

--- TGbot/TGbot/settings.py
+++ TGbot/TGbot/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-bz$x!qv^4agz%s)j+^sk%dr462+mrasm$u%&!dc(9hs2qq)1p^'
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-fallback-key-not-for-production')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
